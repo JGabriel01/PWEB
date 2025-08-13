8463fcb0a1988df949f49886864399892aa1506b
José Gabriel Cavalcante de Almeida <jgca1@aluno.ifal.edu.br>
2025-08-13 08:30:45 -0300

    update PWEB-922A/anotacoesNodejs/aula4modulos/Export.mjs

--- PWEB-922A/anotacoesNodejs/aula4modulos/Export.mjs
+++ PWEB-922A/anotacoesNodejs/aula4modulos/Export.mjs
@@ -1,3 +1,3 @@
-export function soma(n){
-  return n+n
+export function soma(n,b){
+  return n+b
 } /*Usamos o método de exportação de módulos export somente em arquivos .mjs, já o import pode ser usado em arquivos .js padrão.*/
