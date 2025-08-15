660781f84db3e00cb514f6fa6b51be979d8d5b88
Aluno IFAL <alunoifal@MACEIO.IFAL>
2025-08-15 16:03:34 -0300

    Criando servidor simples

--- PWEB-922A/servidor/index.js
+++ PWEB-922A/servidor/index.js
@@ -1,7 +1,18 @@
-const express = require('express')
+// 1. Importar o Express
+const express = require('express');
 
-//Cria uma instância do Express, que é o nosso servidor
+// 2. Criar uma instância do Express
 const app = express();
 
-// Define a porta em que o servidor irá rodar
+// 3. Definir a porta do servidor
 const port = 3000;
+
+// 4. Criar uma rota para a URL raiz ('/')
+app.get('/', (req, res) => {
+  res.send('Olá! Este é meu primeiro servidor web com Express.');
+});
+
+// 5. Iniciar o servidor e escutar na porta definida
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
